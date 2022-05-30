75d59d8e54be23ef51b7676c3bd0b362f0967cda
Kostia Chornobuk <kostiantyn.chornobuk@gmail.com>
2022-05-30 18:11:31 +0300

    create base spring boot app

---
+++ src/main/java/com/ua/statosudiscord/StatosuDiscordApplication.java
@@ -0,0 +1,13 @@
+package com.ua.statosudiscord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatosuDiscordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatosuDiscordApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ua/statosudiscord/StatosuDiscordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ua.statosudiscord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatosuDiscordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
