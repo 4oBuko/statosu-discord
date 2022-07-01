9dc60736a4f7cc475560e9db82c1a982d81cf432
Kostia Chornobuk <kostiantyn.chornobuk@gmail.com>
2022-07-01 19:43:47 +0300

    add dependency and connect db

---
+++ src/main/java/com/ua/statosudiscord/persistence/repositories/StatisticRepository.java
@@ -0,0 +1,7 @@
+package com.ua.statosudiscord.persistence.repositories;
+
+import com.ua.statosudiscord.persistence.entities.Statistic;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface StatisticRepository extends MongoRepository<Statistic, Statistic> {
+}
