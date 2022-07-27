3fd8490f37a96f9d51833b72fae25b9689251981
Kostia Chornobuk <kostiantyn.chornobuk@gmail.com>
2022-07-27 20:55:18 +0300

    added error handling in OsuAPI

--- src/main/java/com/ua/statosudiscord/apirequests/requests/OsuAPI.java
+++ src/main/java/com/ua/statosudiscord/apirequests/requests/OsuAPI.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
@@ -27,16 +28,21 @@ public class OsuAPI {
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", tokenManager.getToken().getTokenType() + " " + tokenManager.getToken().getToken());
         HttpEntity httpEntity = new HttpEntity(headers);
-        ResponseEntity<Statistic> response = restTemplate.exchange(url, HttpMethod.GET, httpEntity, Statistic.class, username);
-        if (response.getStatusCode() == HttpStatus.OK) {
-            Statistic statistic = response.getBody();
-            HttpHeaders responseHeaders = response.getHeaders();
-            statistic.setLastUpdated(LocalDateTime.ofEpochSecond(responseHeaders.getDate() / 1000, 0, ZoneOffset.UTC));
-            return response.getBody();
-        } else {
-            System.out.println("Error happened");
-            return null;
+        try {
+            ResponseEntity<Statistic> response = restTemplate.exchange(url, HttpMethod.GET, httpEntity, Statistic.class, username);
+            if (response.getStatusCode() == HttpStatus.OK) {
+                Statistic statistic = response.getBody();
+                HttpHeaders responseHeaders = response.getHeaders();
+                statistic.setLastUpdated(LocalDateTime.ofEpochSecond(responseHeaders.getDate() / 1000, 0, ZoneOffset.UTC));
+                return response.getBody();
+            } else {
+                System.out.println("Error happened.Status code: " + response.getStatusCode());
+                return null;
+            }
+        } catch (RestClientException e) {
+            e.printStackTrace();
         }
+        return null;
     }
 
 //    todo:create method fog get multiple users when endpoint will be available
