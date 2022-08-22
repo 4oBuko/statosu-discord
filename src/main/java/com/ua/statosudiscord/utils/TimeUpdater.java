c5e18a92fa6d3bd5ff4e69ca3f41f83f9682ea51
Kostia Chornobuk <kostiantyn.chornobuk@gmail.com>
2022-08-22 23:35:25 +0300

    1st version of time converter (isn't tested)

--- src/main/java/com/ua/statosudiscord/utils/TimeUpdater.java
+++ src/main/java/com/ua/statosudiscord/utils/TimeUpdater.java
@@ -3,24 +3,58 @@ package com.ua.statosudiscord.utils;
 import com.ua.statosudiscord.persistence.entities.Statistic;
 import com.ua.statosudiscord.persistence.entities.UpdatePeriod;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class TimeUpdater {
     public static LocalDateTime getNextUpdateTime(Statistic statistic) {
-        LocalDate localDate = statistic.getLastUpdated().toLocalDate();
+//      todo: check if I need a value for local date
+//      todo: check if difference between previous update and planned update
+//         (because system wasn't working) is more than 1 or 2 hours
+//        if more, two updates in one day
+        if (statistic.getNextUpdateTime() != null &&
+                statistic.getLastUpdated().toLocalDate().equals(statistic.getNextUpdateTime().toLocalDate()) &&
+                statistic.getNextUpdateTime().getHour() - statistic.getLastUpdated().getHour() >= 2) {
+            return statistic.getNextUpdateTime();
+        }
+        LocalDate lastUpdated = statistic.getLastUpdated().toLocalDate();
         LocalTime time = LocalTime.MIDNIGHT;
-        time = time.plusHours(statistic.getUpdateHour());
-        localDate = localDate.plusDays(
-                statistic.getPeriod().compareTo(UpdatePeriod.daily) == 0 ? 1 : 0
-        );
-        localDate = localDate.plusWeeks(
-                statistic.getPeriod().compareTo(UpdatePeriod.weekly) == 0 ? 1 : 0
-        );
-        localDate = localDate.plusMonths(
-                statistic.getPeriod().compareTo(UpdatePeriod.monthly) == 0 ? 1 : 0
-        );
-        return LocalDateTime.of(localDate, time);
+        time = time.plusHours(statistic.getUser().getUpdateTime());
+        if (statistic.getUser().getUpdatePeriod().equals(UpdatePeriod.daily)) {
+            lastUpdated = lastUpdated.plusDays(1);
+        } else if (statistic.getUser().getUpdatePeriod().equals(UpdatePeriod.weekly)) {
+            DayOfWeek lastUpdatedDay = statistic.getLastUpdated().getDayOfWeek();
+            DayOfWeek updateDay = statistic.getUser().getDayOfWeek();
+            int daysToNextUpdate;
+//           find number of days to the next update
+            if (updateDay.compareTo(lastUpdatedDay) >= 0) {
+                daysToNextUpdate = updateDay.getValue() - lastUpdatedDay.getValue();
+            } else {
+                daysToNextUpdate = 7 - (lastUpdatedDay.getValue() - updateDay.getValue());
+            }
+            lastUpdated = lastUpdated.plusDays(daysToNextUpdate);
+        } else if (statistic.getUser().getUpdatePeriod().equals(UpdatePeriod.monthly)) {
+            if (statistic.getNextUpdateTime().getMonth() == statistic.getLastUpdated().getMonth() &&
+                    statistic.getUser().getDayOfMonth() > statistic.getLastUpdated().getDayOfMonth()
+            ) {
+                lastUpdated = lastUpdated.plusDays(statistic.getUser().getDayOfMonth() - statistic.getLastUpdated().getDayOfMonth());
+            } else {
+                if (lastUpdated.getMonth().getValue() == 12) {
+                    lastUpdated = LocalDate.of(LocalDate.now().getYear() + 1, 1, statistic.getUser().getDayOfMonth());
+                } else {
+                    lastUpdated = LocalDate.of(LocalDate.now().getYear(), LocalDate.now().getMonth().getValue() + 1, statistic.getUser().getDayOfMonth());
+                }
+            }
+//            if the number of the month is bigger than previous update time (15 and 12)
+//            just add days
+
+//            if the previous update day is less than update day and the month is the same
+//            add new month to the date
+
+//                day can be less than next update
+        }
+        return LocalDateTime.of(lastUpdated, time);
     }
 }
--- src/test/java/com/ua/statosudiscord/services/TimeUpdaterTest.java
+++ src/test/java/com/ua/statosudiscord/services/TimeUpdaterTest.java
@@ -3,24 +3,26 @@ package com.ua.statosudiscord.services;
 import com.ua.statosudiscord.persistence.builders.StatisticBuilder;
 import com.ua.statosudiscord.persistence.entities.Statistic;
 import com.ua.statosudiscord.persistence.entities.UpdatePeriod;
+import com.ua.statosudiscord.persistence.entities.User;
 import com.ua.statosudiscord.utils.TimeUpdater;
 import org.junit.jupiter.api.Test;
 
+import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TimeUpdaterTest {
 
+    User user = new User(null, null, null, null, UpdatePeriod.weekly, DayOfWeek.MONDAY, 0, 22);
     StatisticBuilder statisticBuilder = new StatisticBuilder()
-            .setUpdateHour(22)
-            .setPeriod(UpdatePeriod.weekly)
-            .setLastUpdated(LocalDateTime.of(2022,10,12,12,12));
+            .setUser(user)
+            .setLastUpdated(LocalDateTime.of(2022, 10, 12, 12, 12));
 
     Statistic statistic = statisticBuilder.build();
 
     @Test
     public void getNextUpdateTime() {
-        assertEquals(LocalDateTime.of(2022,10,19,22, 0), TimeUpdater.getNextUpdateTime(statistic));
+        assertEquals(LocalDateTime.of(2022, 10, 17, 22, 0), TimeUpdater.getNextUpdateTime(statistic));
     }
 }
