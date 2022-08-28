fb21eb56481c64cb0846b954e90d6c750560cacc
Kostia Chornobuk <kostiantyn.chornobuk@gmail.com>
2022-08-28 12:05:50 +0300

    added logic for updating statistic

--- src/main/java/com/ua/statosudiscord/bot/commands/handlers/UpdateCommandHandler.java
+++ src/main/java/com/ua/statosudiscord/bot/commands/handlers/UpdateCommandHandler.java
@@ -1,15 +1,33 @@
 package com.ua.statosudiscord.bot.commands.handlers;
 
+import com.ua.statosudiscord.persistence.entities.Statistic;
+import com.ua.statosudiscord.persistence.entities.UpdatePeriod;
+import com.ua.statosudiscord.persistence.entities.User;
+import com.ua.statosudiscord.services.StatisticService;
+import com.ua.statosudiscord.services.UserService;
+import com.ua.statosudiscord.utils.MessageBuilder;
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import discord4j.core.object.command.ApplicationCommandInteractionOption;
+import discord4j.core.object.command.ApplicationCommandInteractionOptionValue;
 import discord4j.core.object.command.ApplicationCommandOption;
 import discord4j.discordjson.json.ApplicationCommandOptionChoiceData;
 import discord4j.discordjson.json.ApplicationCommandOptionData;
 import discord4j.discordjson.json.ApplicationCommandRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
+import java.time.DayOfWeek;
+
 @Component
 public class UpdateCommandHandler extends CommandHandler {
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    StatisticService statisticService;
+
     public UpdateCommandHandler() {
         ApplicationCommandRequest commandRequest = ApplicationCommandRequest.builder()
                 .name("update")
@@ -116,6 +134,57 @@ public class UpdateCommandHandler extends CommandHandler {
 
     @Override
     public Mono<Void> handle(ChatInputInteractionEvent event) {
-        return null;
+        String response = "";
+        long channelId = event.getInteraction().getChannelId().asLong();
+        long userId = event.getInteraction().getUser().getId().asLong();
+//        arguments[1] = arguments[1].toLowerCase();
+        User existedUser = userService.getUser(channelId, userId);
+        int updateTime = 0;
+        int numberOfMonth = 0;
+        DayOfWeek dayOfWeek = null;
+        UpdatePeriod updatePeriod;
+        ApplicationCommandInteractionOption updatePeriodOption;
+        if (event.getOption("weekly").isPresent()) {
+            updatePeriodOption = event.getOption("weekly").get();
+            updatePeriod = UpdatePeriod.weekly;
+        } else if (event.getOption("monthly").isPresent()) {
+            updatePeriodOption = event.getOption("monthly").get();
+            updatePeriod = UpdatePeriod.monthly;
+        } else {
+            updatePeriodOption = event.getOption("daily").get();
+            updatePeriod = UpdatePeriod.daily;
+        }
+        if (updatePeriodOption.getOption("day-of-week").isPresent()) {
+            dayOfWeek = DayOfWeek.valueOf(updatePeriodOption.getOption("day-of-week")
+                    .flatMap(ApplicationCommandInteractionOption::getValue)
+                    .map(ApplicationCommandInteractionOptionValue::asString)
+                    .get().toUpperCase()
+            );
+        }
+        if (updatePeriodOption.getOption("day").isPresent()) {
+            numberOfMonth = updatePeriodOption.getOption("day")
+                    .flatMap(ApplicationCommandInteractionOption::getValue)
+                    .map(ApplicationCommandInteractionOptionValue::asLong)
+                    .get()
+                    .intValue();
+        }
+        updateTime = updatePeriodOption.getOption("time")
+                .flatMap(ApplicationCommandInteractionOption::getValue)
+                .map(ApplicationCommandInteractionOptionValue::asLong)
+                .get().intValue();
+        if (existedUser == null) {
+            response = "You cannot use this operation. Use !nickname command first";
+        } else {
+            existedUser.setUpdatePeriod(updatePeriod);
+            existedUser.setUpdateTime(updateTime);
+            existedUser.setDayOfMonth(numberOfMonth);
+            existedUser.setDayOfWeek(dayOfWeek);
+            User updatedUser = userService.changeUpdateInto(existedUser);
+            Statistic statistic = statisticService.getNewestStatistic(updatedUser);
+            response = MessageBuilder.createMessage(statistic).getMessage();
+        }
+        return event.reply()
+                .withEphemeral(true)
+                .withContent(response);
     }
 }
