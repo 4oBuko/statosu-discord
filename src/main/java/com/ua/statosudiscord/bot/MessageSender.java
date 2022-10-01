3bc6faf51cf10dc746883c37fd296bf43122752e
Kostia Chornobuk <kostiantyn.chornobuk@gmail.com>
2022-10-01 21:40:19 +0300

    add new method for sending messages

--- src/main/java/com/ua/statosudiscord/bot/MessageSender.java
+++ src/main/java/com/ua/statosudiscord/bot/MessageSender.java
@@ -18,8 +18,15 @@ public class MessageSender {
     public void sendMessageInChannelWithUserMention(Message message) {
         gatewayDiscordClient.getChannelById(Snowflake.of(message.getDiscordChannelId()))
                 .ofType(MessageChannel.class)
-                .flatMap(channel -> channel.createMessage("<@" + message.getDiscordUserId() + ">\n" + message.getMessage()))
+                .flatMap(channel -> channel.createMessage(message.getMessage()))
                 .doOnError(error -> logger.error(error.getMessage()))
+                .subscribe();
+    }
+    public void sendMessageInChannelWithoutMentioningUser(Message message) {
+        gatewayDiscordClient
+                .rest()
+                .getChannelById(Snowflake.of(message.getDiscordChannelId()))
+                .createMessage(message.getMessage())
                 .subscribe();
     }
 }
